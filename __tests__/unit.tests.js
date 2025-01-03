141996a198d1df890758a65375de85245bfe50fb
Simon Highfield <highfieldsimon@gmail.com>
2025-01-03 16:02:36 +0000

    Refactored so generateDeck() runs once beforeAll tests, and each tests are on deck.forEach((card) ...

--- __tests__/unit.tests.js
+++ __tests__/unit.tests.js
@@ -9,56 +9,64 @@ describe("generateDeck() of cards", () => {
         });
     })
 
+
     describe("Card properties", () => {
+
+        let deck = []
+        beforeAll(() => {
+            deck = generateDeck();
+        });
+
         test("A card is an object", () => {
-            const deck = generateDeck();
-            const card = deck[0]
-            expect(typeof card).toBe('object');
+            deck.forEach((card)=> {
+                expect(typeof card).toBe('object');
+            })
         });
         describe("Card suit", () => {
             test("A card has a suit", () => {
-                const deck = generateDeck();
-                const card = deck[0]
-                expect(card).toHaveProperty('suit');
+                deck.forEach((card)=> {
+                    expect(card).toHaveProperty('suit');
+                })
             });
             test("A card's suit is one of clubs, diamonds, hearts, spades", () => {
-                const deck = generateDeck();
-                const card = deck[0]
-                expect(card.suit).toBeOneOf(['clubs', 'diamonds', 'hearts', 'spades']);
+                deck.forEach((card)=> {
+                    expect(card.suit).toBeOneOf(['clubs', 'diamonds', 'hearts', 'spades']);
+                })
             });
         })
         describe("Card values", () => {
             test("A card's values are in a valid array", () => {
-                const deck = generateDeck();
-                const card = deck[0]
-                expect(card).toHaveProperty('values');
-                expect(card.values).toBeArray();
-                expect(card.values.length).toBeWithin(1,3)
+                deck.forEach((card)=> {
+                    expect(card).toHaveProperty('values');
+                    expect(card.values).toBeArray();
+                    expect(card.values.length).toBeWithin(1,3)
+                })
             });
             test("A card's values are between 1 and 11 ", () => {
-                const deck = generateDeck();
-                const card = deck[0]
-                const values = card.values
-        
-                values.forEach(value => {
-                    expect(value).toBeNumber()
-                    expect(value).not.toBeNaN()
-                    expect(value).toBeWithin(1,12)
-                });
+                deck.forEach((card)=> {
+                    const values = card.values
+
+                    values.forEach(value => {
+                        expect(value).toBeNumber()
+                        expect(value).not.toBeNaN()
+                        expect(value).toBeWithin(1,12)
+                    });
+                })
             });
         })
         describe("Card name", () => {
             test("A card has a name", () => {
-                const deck = generateDeck();
-                const card = deck[0]
-                expect(card).toHaveProperty('name');
+                deck.forEach((card)=> {
+                    expect(card).toHaveProperty('name');
+                })
             });
             test("A card's name is a string such as Ace, Two, Three, Jack, ... etc", () => {
-                const deck = generateDeck();
-                const card = deck[0]
                 const validNames = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
 
-                expect(card.name).toBeOneOf(validNames);
+                deck.forEach((card)=> {
+                    expect(card.name).toBeOneOf(validNames);
+                })
+                
             });
         })
     })
