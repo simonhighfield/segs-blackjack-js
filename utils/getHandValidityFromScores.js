12b8f2456174f7281c5c0204320e020bbe1bdd6c
Simon Highfield <highfieldsimon@gmail.com>
2025-01-10 21:15:13 +0000

    refactored to errorCheckScores

--- utils/getHandValidityFromScores.js
+++ utils/getHandValidityFromScores.js
@@ -2,12 +2,20 @@ const errorCheckArray = require("./errorCheckArray")
 
 module.exports = getHandValidityFromScores = (inputScores) => {
 
-    errorCheckArray(inputScores, 'scores')
+    errorCheckScores(inputScores);
+
+    
+
+
+}
+
+function errorCheckScores(inputScores) {
+    errorCheckArray(inputScores, 'scores');
 
     inputScores.forEach(score => {
-        if (typeof score !== 'number') {            
+        if (typeof score !== 'number') {
             throw new TypeError("'scores' should be numbers");
-        } 
+        }
         else if (score < 2) {
             throw new TypeError("'scores' should be at least 2");
         }
